170905713129a7d5b71417d744d5a217b497d2c4
Altiansyah Fanny <altiansyahfanny21@gmail.com>
2022-03-28 14:49:34 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import instance from './axios'
+import Banner from './compenents/Banner';
+import Navbar from './compenents/Navbar';
+import Row from './compenents/Row'
+import requests from './request'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	// const [data, setData] = useState({});
+	// useEffect(() => {
+	// 	instance.get(requests.fetchTrending).then((res) => setData(res));
+	// }, [])
+
+	// console.log(data)
+
+	return (
+		<div className='app'>
+			<Navbar />
+			<Banner />
+
+			<Row title={'NETFLIX ORIGINALS'} fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+			<Row title={'Trending Now'} fetchUrl={requests.fetchTrending} isLargeRow={false} />
+			<Row title={'Action Movies'} fetchUrl={requests.fetchActionMovies} isLargeRow={false} />
+			<Row title={'Comedy Movies'} fetchUrl={requests.fetchComedyMovies} isLargeRow={false} />
+			<Row title={'Horror Movies'} fetchUrl={requests.fetchHorrorovies} isLargeRow={false} />
+			<Row title={'Romance Movies'} fetchUrl={requests.fetchRomanceovies} isLargeRow={false} />
+			<Row title={'Documentaries'} fetchUrl={requests.fetchDocumentaries} isLargeRow={false} />
+		</div>
+	)
 }
 
-export default App;
+export default App
